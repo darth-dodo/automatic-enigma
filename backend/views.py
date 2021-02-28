c74ee5032a68485cb93dfd4828aeadba7a07bc92
Abhishek Juneja <darth-dodo@users.noreply.github.com>
2021-02-28 18:05:53 +0530

    Deploy Automation (#1)

--- backend/views.py
+++ backend/views.py
@@ -1,9 +1,12 @@
 from django.http import JsonResponse
 import datetime
 import pytz
+from django.core.management.utils import get_random_secret_key
+
 
 def ping(request):
     data = dict()
     data["ping"] = "pong"
     data["timestamp"] = datetime.datetime.now(tz=pytz.timezone("Asia/Kolkata")).isoformat()
+    data["random_string"] = get_random_secret_key()
     return JsonResponse(data)
